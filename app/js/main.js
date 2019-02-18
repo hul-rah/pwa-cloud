1d208331f34f2a1d1a858f583282854067a54c61
rogueSentinel <pwacloudspace@gmail.com>
2019-02-17 19:14:03 -0700

    Added support for app install banner

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,18 @@ const dbPromise = createIndexedDB();
 
 loadContentNetworkFirst();
 
+var deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', function (e) {
+        console.log('beforeinstallprompt listener initialized ');
+        console.log(e);
+        // Prevent Chrome 67 and earlier from automatically showing the prompt
+        e.preventDefault();
+        // Stash the event so it can be triggered later.
+        deferredPrompt = e;
+        showAddToHomeScreen();
+});
+
 function loadContentNetworkFirst() {
     getServerData()
         .then(dataFromNetwork => {
@@ -283,6 +295,29 @@ function getMaxPk() {
 
 
 }
+
+function addToHomeScreen() {
+        let a2hsBtn = document.querySelector(".ad2hs-prompt");  // hide our user interface that shows our A2HS button
+        a2hsBtn.style.display = 'none';  // Show the prompt
+        deferredPrompt.prompt();  // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice
+          .then(function(choiceResult){
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
+}
+
+function showAddToHomeScreen() {
+        let a2hsBtn = document.querySelector(".ad2hs-prompt");
+        a2hsBtn.style.display = "block";
+        a2hsBtn.addEventListener("click", addToHomeScreen);
+}
+
+
 
 
 this.addEventListener('activate', function(e) {
